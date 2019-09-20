aba598a8cb10707e8074461d583730ff1b234ac4
Erik Berggren <94erbe28@gmail.com>
2019-09-20 13:58:48 +0200

    added basic structure

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+var paths = {
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'assets/css/'
+    },
+    scripts: {
+        src: 'src/scripts/*.js',
+        dest: 'assets/scripts/'
+    }
+};
+
+gulp.task('sass', function () {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.styles.src, ['sass']);
+});
