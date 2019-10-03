1c4f9e20497a43e8e1fce61c526d80a252017a1a
Erik Berggren <94erbe28@gmail.com>
2019-10-03 11:05:19 +0200

    finished merging to php structure + slideshow

--- template/section.php
+++ template/section.php
@@ -10,5 +10,5 @@ echo '<div class="section">
                 </div>
                 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
                 <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>';
+</div></div>';
 ?>
